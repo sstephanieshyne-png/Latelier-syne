b0b897054840a8d0ff2495f1a5f4d02746f50647
sstephanieshyne-png <sstephanieshyne@gmail.com>
2026-09-01 18:48:34 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,5 @@
 let products = [];
+console.log("ADMIN JS LOADED");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
